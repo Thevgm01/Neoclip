6e3040e83c5366b09dc627c376ad5dca7ba49e17
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-12 18:10:46 -0500

    Cool optimization for detecting collider type

--- Assets/Utils.cs
+++ Assets/Utils.cs
@@ -11,23 +11,14 @@ public class Utils
 
     public static float CalculateVolume(Collider collider)
     {
-        if (collider is BoxCollider)
+        switch (collider)
         {
-            BoxCollider boxCollider = collider as BoxCollider;
-            return boxCollider.size.x * boxCollider.size.y * boxCollider.size.z;
-        }
-        
-        if (collider is CapsuleCollider)
-        {
-            CapsuleCollider capsuleCollider = collider as CapsuleCollider;
-            return Mathf.PI * capsuleCollider.radius * capsuleCollider.radius *
-                   (capsuleCollider.radius * 4.0f / 3.0f + capsuleCollider.height);
-        }
-
-        if (collider is SphereCollider)
-        {
-            SphereCollider sphereCollider = collider as SphereCollider;
-            return Mathf.PI * sphereCollider.radius * sphereCollider.radius * sphereCollider.radius * 4.0f / 3.0f;
+            case BoxCollider boxCollider:
+                return boxCollider.size.x * boxCollider.size.y * boxCollider.size.z;
+            case CapsuleCollider capsuleCollider:
+                return Mathf.PI * capsuleCollider.radius * capsuleCollider.radius * (capsuleCollider.radius * 4.0f / 3.0f + capsuleCollider.height);
+            case SphereCollider sphereCollider:
+                return Mathf.PI * sphereCollider.radius * sphereCollider.radius * sphereCollider.radius * 4.0f / 3.0f;
         }
 
         Debug.LogWarning($"Utils.CalculateVolume: Unknown collider {collider}");
