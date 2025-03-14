40e3152e271352adceabbdfdd597551079e43496
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-13 21:44:50 -0500

    Created a separate renderer for the drag camera

--- Assets/DragCamera.cs
+++ Assets/DragCamera.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DragCamera : MonoBehaviour
 {
     [SerializeField] private RagdollAverages ragdollAverages;
     [SerializeField] private float minSpeed = 1.0f;
     private Camera camera;
-
+    
     private void Awake()
     {
         camera = GetComponent<Camera>();
@@ -21,8 +23,11 @@ public class DragCamera : MonoBehaviour
             transform.SetPositionAndRotation(
                 ragdollAverages.AveragePosition + rotation * new Vector3(0, 0, -camera.orthographicSize), 
                 rotation);
-            
-            camera.Render();
+
+            camera.Render(); 
+            //var request = new UniversalRenderPipeline.SingleCameraRequest();
+            //request.destination = rt;
+            //RenderPipeline.SubmitRenderRequest(camera, a);
         }
     }
 }
