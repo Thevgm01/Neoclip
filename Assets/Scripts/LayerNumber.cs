d0e9efefaeeb93a221400e8f25938de4497d2264
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-17 20:48:28 -0500

    Merged LayerNumberDrawer into primary file

--- Assets/Scripts/LayerNumber.cs
+++ Assets/Scripts/LayerNumber.cs
@@ -1,8 +1,19 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 [Serializable]
 public struct LayerNumber
 {
     public int value;
+}
+
+[CustomPropertyDrawer(typeof(LayerNumber))]
+public class LayerNumberDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        SerializedProperty layerNumber = property.FindPropertyRelative("value");
+        layerNumber.intValue = EditorGUI.LayerField(position, label, layerNumber.intValue);
+    }
 }
