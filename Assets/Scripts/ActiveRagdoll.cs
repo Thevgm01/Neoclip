acb21eb44c06f25beb71589e72b19e4f71f06db3
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-20 19:40:02 -0500

    Extracted BonePair into its own class

--- Assets/Scripts/ActiveRagdoll.cs
+++ Assets/Scripts/ActiveRagdoll.cs
@@ -8,61 +8,6 @@ public class ActiveRagdoll : MonoBehaviour
     [SerializeField] private Transform driverSkeleton;
     [SerializeField] private Transform ragdollSkeleton;
     
-    private class BonePair
-    {
-        public Transform AnimatedBone { get; }
-        public Transform DrivenBone { get; }
-        
-        public BonePair(Transform animatedBone, Transform drivenBone)
-        {
-            this.AnimatedBone = animatedBone;
-            this.DrivenBone = drivenBone;
-        }
-        
-        public BonePair(BonePair other) : this(other.AnimatedBone, other.DrivenBone) {}
-        
-        public override string ToString()
-        {
-            return AnimatedBone.name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            BonePair other = obj as BonePair;
-            return other != null && other.AnimatedBone == AnimatedBone && other.DrivenBone == DrivenBone;
-        }
-
-        public override int GetHashCode()
-        {
-            return AnimatedBone.GetHashCode() ^ DrivenBone.GetHashCode();
-        }
-        
-        public virtual void SetRotation() => DrivenBone.localRotation = AnimatedBone.localRotation;
-    }
-
-    private class JointBonePair : BonePair
-    {
-        private readonly ConfigurableJoint joint;
-
-        private readonly Quaternion worldToStartSpace;
-        private readonly Quaternion jointToWorldSpace;
-        
-        public JointBonePair(BonePair original, ConfigurableJoint joint) : base(original)
-        {
-            this.joint = joint;
-
-            // https://gist.github.com/mstevenson/7b85893e8caf5ca034e6
-            var forward = Vector3.Cross(joint.axis, joint.secondaryAxis).normalized;
-            var up = Vector3.Cross(forward, joint.axis).normalized;
-            Quaternion worldToJointSpace = Quaternion.LookRotation(forward, up);
-            jointToWorldSpace = Quaternion.Inverse(worldToJointSpace);
-            worldToStartSpace = DrivenBone.localRotation * worldToJointSpace;
-        }
-
-        public override void SetRotation() =>
-            joint.targetRotation = jointToWorldSpace * Quaternion.Inverse(AnimatedBone.localRotation) * worldToStartSpace;
-    }
-    
     private HashSet<BonePair> basicBonePairs;
     private List<JointBonePair> jointBonePairs;
     
