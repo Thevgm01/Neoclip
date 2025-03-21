008caf978449c0689455fc4d8b41dcd7bf35726c
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-21 18:54:00 -0500

    Raycasts should be stopping on the last backface
    But I think it's off since the results are unordered

--- Assets/Scripts/ExitDirectionFinder.cs
+++ Assets/Scripts/ExitDirectionFinder.cs
@@ -12,14 +12,42 @@ public class ExitDirectionFinder : MonoBehaviour
     [SerializeField] private LayerMask layerMask;
     
     private readonly Vector3[] feelerDirections = new Vector3[NUM_RAYS];
-    
-    #if UNITY_EDITOR
+
+#if UNITY_EDITOR
         private void OnDrawGizmosSelected()
         {
             RunRaycasts();
         }
-    #endif
+#endif
 
+    private bool GetIndexOfLastInsideHit(NativeArray<RaycastHit> results, int startIndex, out int backfaceIndex)
+    {
+        backfaceIndex = -1;
+
+        for (int i = 0; i < NUM_HITS; i++)
+        {
+            RaycastHit hit = results[startIndex * NUM_HITS + i];
+
+            if (hit.collider == null)
+            {
+                return backfaceIndex >= 0;
+            }
+
+            float dot = Vector3.Dot(hit.normal, feelerDirections[i]);
+
+            if (backfaceIndex == -1 && dot > 0)
+            {
+                backfaceIndex = i;
+            }
+            else if (backfaceIndex >= 0 && dot <= 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
     private void RunRaycasts()
     {
         NativeArray<RaycastHit> results = new NativeArray<RaycastHit>(NUM_RAYS * NUM_HITS, Allocator.TempJob);
@@ -47,22 +75,11 @@ public class ExitDirectionFinder : MonoBehaviour
 
         for (int i = 0; i < NUM_RAYS; i++)
         {
-            for (int j = 0; j < NUM_HITS; j++)
+            if (GetIndexOfLastInsideHit(results, i, out int backfaceIndex))
             {
-                RaycastHit hit = results[i * NUM_HITS + j];
-
-                if (hit.collider == null)
-                {
-                    break;
-                }
-                
-                #if UNITY_EDITOR
-                    Gizmos.color = new Color32(0, (byte)((j + 1) * 32), 0, 255);
-                    Gizmos.DrawLine(
-                        j == 0 ? origin : results[i * NUM_HITS + j - 1].point, 
-                        hit.point);
-                #endif
-                //Debug.DrawLine(origin, results[i * NUM_HITS].point);
+#if UNITY_EDITOR
+                Gizmos.DrawLine(origin, results[i * NUM_HITS + backfaceIndex].point);
+#endif
             }
         }
 
