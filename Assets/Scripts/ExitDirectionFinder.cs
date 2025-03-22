44a5394693a4fd92d77615174c8f54fa31734b49
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-03-21 21:58:21 -0500

    Turned the whole raycast system into a job
    It's... instantaneous now? Wow!

--- Assets/Scripts/ExitDirectionFinder.cs
+++ Assets/Scripts/ExitDirectionFinder.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
-using UnityEngine.Profiling;
 
 public class ExitDirectionFinder : MonoBehaviour
 {
@@ -12,78 +10,123 @@ public class ExitDirectionFinder : MonoBehaviour
     [SerializeField] private float distance = 100.0f;
     [SerializeField] private LayerMask layerMask;
     
-    private readonly Vector3[] rayDirections = new Vector3[NUM_RAYS];
-
-#if UNITY_EDITOR
-        private void OnDrawGizmosSelected()
-        {
-            RunRaycasts();
-        }
-#endif
+    private readonly NativeArray<Vector3> rayDirections = new (NUM_RAYS, Allocator.Persistent);
+    private readonly NativeArray<RaycastHit> results = new (NUM_RAYS, Allocator.Persistent);
+    private readonly NativeArray<RaycastCommand> commands = new (NUM_RAYS, Allocator.Persistent);
     
-    private void RunRaycasts()
+    public struct RayCreationParameters
     {
-        NativeArray<RaycastHit> results = new NativeArray<RaycastHit>(NUM_RAYS, Allocator.TempJob);
-        NativeArray<RaycastCommand> commands = new NativeArray<RaycastCommand>(NUM_RAYS, Allocator.TempJob);
+        public Vector3 origin;
+        public NativeArray<Vector3> directions;
+        
+        public QueryParameters query;
+        public float distance;
+    }
+    
+    [BurstCompile]
+    public struct CreateOutRays : IJob
+    {
+        private NativeArray<RaycastCommand> commands;
+        [ReadOnly] private RayCreationParameters rayParameters;
 
-        Vector3 origin = transform.position;
+        public CreateOutRays(NativeArray<RaycastCommand> commands, RayCreationParameters rayParameters)
+        {
+            this.commands = commands;
+            this.rayParameters = rayParameters;
+        }
         
+        public void Execute()
+        {
+            for (int i = 0; i < NUM_RAYS; i++)
+            {
+                commands[i] = new RaycastCommand(
+                    rayParameters.origin,
+                    rayParameters.directions[i],
+                    rayParameters.query,
+                    rayParameters.distance);
+            }
+        }
+    }
+    
+    [BurstCompile]
+    private struct CreateInRays : IJob
+    {
+        [ReadOnly] private RayCreationParameters rayParameters;
+        [ReadOnly] private NativeArray<RaycastHit> hits;
+        private NativeArray<RaycastCommand> commands;
+        
+        public CreateInRays(NativeArray<RaycastCommand> commands, NativeArray<RaycastHit> hits, RayCreationParameters rayParameters)
+        {
+            this.commands = commands;
+            this.hits = hits;
+            this.rayParameters = rayParameters;
+        }
+        
+        public void Execute()
+        {
+            for (int i = 0; i < NUM_RAYS; i++)
+            {
+                //if (hits[i].collider == null) // Can't do this in a job!
+                if (hits[i].point == default) // Hopefully this hack works
+                {
+                    commands[i] = new RaycastCommand(
+                        rayParameters.origin + rayParameters.directions[i] * rayParameters.distance,
+                        -rayParameters.directions[i],
+                        rayParameters.query,
+                        rayParameters.distance);
+                }
+                else
+                {
+                    commands[i] = new RaycastCommand(
+                        hits[i].point,
+                        -rayParameters.directions[i],
+                        rayParameters.query,
+                        hits[i].distance);
+                }
+            }
+        }
+    }
+    
+    public JobHandle ScheduleJobs()
+    {
         transform.TransformDirections(UniformSpherePoints.GetCachedVectors(NUM_RAYS), rayDirections);
-        
-        // Cast outward
-        QueryParameters outParameters = new QueryParameters()
+
+        RayCreationParameters rayParameters = new RayCreationParameters
         {
-            layerMask = layerMask.value,
-            hitBackfaces = false
+            origin = transform.position,
+            directions = rayDirections,
+            query = new QueryParameters
+            {
+                layerMask = layerMask.value,
+                hitMultipleFaces = false,
+                hitTriggers = QueryTriggerInteraction.UseGlobal,
+                hitBackfaces = false
+            },
+            distance = distance
         };
         
-        for (int i = 0; i < NUM_RAYS; i++)
-        {
-            commands[i] = new RaycastCommand(origin, rayDirections[i], outParameters, distance);
-        }
-                
-        JobHandle handle = RaycastCommand.ScheduleBatch(commands, results, 1, 1);
-        handle.Complete();
+        JobHandle createOutRays = new CreateOutRays(commands, rayParameters).Schedule();
+        JobHandle castOutRays = RaycastCommand.ScheduleBatch(commands, results, 1, 1, createOutRays);
+        JobHandle createInRays = new CreateInRays(commands, results, rayParameters).Schedule(castOutRays);
+        JobHandle castInRays = RaycastCommand.ScheduleBatch(commands, results, 1, 1, createInRays);
         
-        // Cast inward
-        QueryParameters inParameters = new QueryParameters()
-        {
-            layerMask = layerMask.value,
-            hitBackfaces = false
-        };
-        
-        for (int i = 0; i < NUM_RAYS; i++)
-        {
-            if (results[i].collider == null)
-            {
-                commands[i] = new RaycastCommand(origin + rayDirections[i] * distance, -rayDirections[i], inParameters, distance);
-            }
-            else if (results[i].collider)
-            {
-                commands[i] = new RaycastCommand(results[i].point, -rayDirections[i], inParameters, results[i].distance);
-            }
-        }
-        
-        handle = RaycastCommand.ScheduleBatch(commands, results, 1, 1);
-        handle.Complete();
-        
-        // Process final results
+        return castInRays;
+    }
+    
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        float time = Time.realtimeSinceStartup;
+        ScheduleJobs().Complete();
+        Debug.Log((Time.realtimeSinceStartup - time) / 1000.0f);
+
         for (int i = 0; i < NUM_RAYS; i++)
         {
             if (results[i].collider != null)
             {
-#if UNITY_EDITOR
-                Gizmos.DrawLine(origin, results[i].point);
-#endif
+                Gizmos.DrawLine(transform.position, results[i].point);
             }
         }
-        
-        results.Dispose();
-        commands.Dispose();
     }
-
-    private void FixedUpdate()
-    {
-        RunRaycasts();
-    }
+#endif
 }
