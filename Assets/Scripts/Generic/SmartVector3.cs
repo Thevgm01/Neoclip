8bf51d4291fbc9ae46a5ae22eb190d20b54a2be2
Steven Dunn <30204079+Thevgm01@users.noreply.github.com>
2025-05-03 01:02:21 -0500

    Added operators and a summary to SmartVector3

--- Assets/Scripts/Generic/SmartVector3.cs
+++ Assets/Scripts/Generic/SmartVector3.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/// <summary>
+/// A Vector3 that caches its magnitude and normal vectors when created or modified.
+/// Additionally, the magnitude and normal can be changed independently, and the main Vector3 is automatically adjusted..
+/// </summary>
 public struct SmartVector3
 {
     private Vector3 _vector;
@@ -33,6 +37,7 @@ public struct SmartVector3
         get => _normalized;
         set
         {
+            // Currently we're just trusting that 'value' is actually normalized
             _vector = value * _magnitude;
             // magnitude stays the same
             _normalized = value;
@@ -46,7 +51,7 @@ public struct SmartVector3
         this.Value = vector;
     }
 
-    public SmartVector3(SmartVector3 other) : this()
+    public SmartVector3(SmartVector3 other)
     {
         this._vector = other._vector;
         this._magnitude = other._magnitude;
@@ -55,4 +60,16 @@ public struct SmartVector3
     
     public static implicit operator Vector3(SmartVector3 smartVector) => smartVector.Value;
     public static implicit operator SmartVector3(Vector3 vector) => new SmartVector3(vector);
+    
+    public static SmartVector3 operator +(SmartVector3 a, Vector3 b) => new SmartVector3(a.Value + b);
+    public static SmartVector3 operator +(Vector3 a, SmartVector3 b) => new SmartVector3(a + b.Value);
+    public static SmartVector3 operator -(SmartVector3 a, Vector3 b) => new SmartVector3(a.Value - b);
+    public static SmartVector3 operator -(Vector3 a, SmartVector3 b) => new SmartVector3(a - b.Value);
+    
+    public static SmartVector3 operator +(SmartVector3 a, SmartVector3 b) => new SmartVector3(a.Value + b.Value);
+    public static SmartVector3 operator -(SmartVector3 a, SmartVector3 b) => new SmartVector3(a.Value - b.Value);
+    
+    public static SmartVector3 operator *(SmartVector3 a, float f) => new SmartVector3(a.Value * f);
+    public static SmartVector3 operator *(float f, SmartVector3 a) => new SmartVector3(a.Value * f);
+    public static SmartVector3 operator /(SmartVector3 a, float f) => new SmartVector3(a.Value / f);
 }
